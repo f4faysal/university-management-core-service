9a52e5f4ae42fa23461ed15c0710b3a50512da1a
f4faysal <f4faysals@gmail.com>
2023-08-23 01:26:59 +0600

    getAllFormDB creatrd

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,6 +1,7 @@
 import { AcademicSemester } from "@prisma/client";
 import { Request, Response } from "express";
 import httpStatus from "http-status";
+import { IGenericResponse } from "../../../interfaces/common";
 import catchAsync from "../../../shared/catchAsync";
 import sendResponse from "../../../shared/sendResponse";
 import { academicSemesterService } from "./academicSemester.service";
@@ -16,6 +17,18 @@ const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
      })
 })
 
+const getAllFormDB = catchAsync(async (req: Request, res: Response) => {
+     const result = await academicSemesterService.getAllFormDB();
+
+     sendResponse<IGenericResponse<AcademicSemester[]>>(res, {
+          statusCode: httpStatus.OK,
+          success: true,
+          message: "Academic Semester fetched successfully",
+          data: result
+     })
+})
+
 export const academicSemesterController = {
-     insertIntoDB
+     insertIntoDB,
+     getAllFormDB
 }
--- src/app/modules/academicSemester/academicSemester.routes.ts
+++ src/app/modules/academicSemester/academicSemester.routes.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import validateRequest from "../../middlewares/validateRequest";
 import { academicSemesterController } from "./academicSemester.controller";
+import { academicSemesterValidation } from "./academicSemeter.validation";
 
 const router = express.Router();
 
 
-router.post("/", academicSemesterController.insertIntoDB);
+router.post("/", validateRequest(academicSemesterValidation.create), academicSemesterController.insertIntoDB);
+
+router.get("/", academicSemesterController.getAllFormDB);
 
 export const academicSemesterRoutes = router;
