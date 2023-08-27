e4455bf4851786ebdbe55761bb5508194d21fe90
f4faysal <f4faysals@gmail.com>
2023-08-28 02:22:03 +0600

    add Faculty done

--- src/app/modules/faculty/faculty.routes.ts
+++ src/app/modules/faculty/faculty.routes.ts
@@ -4,7 +4,11 @@ import { FacultyController } from './faculty.controller';
 const router = express.Router();
 
 
+router.get('/', FacultyController.getAllFromDB);
+router.get('/:id', FacultyController.getByIdFromDB);
 router.post('/', FacultyController.insertIntoDB);
+router.patch('/:id', FacultyController.updateOneInDB);
+router.delete('/:id', FacultyController.deteteByIdFromDB);
 
 
 export const FacultyControllerRoutes = router;
